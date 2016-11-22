c983d08ad9ad4dcb70a11af3e7675a9a74211bc7
Keith Turner <kturner@apache.org>
2016-11-22 11:38:16 -0500

    WIP

--- src/main/java/ft/Main.java
+++ src/main/java/ft/Main.java
@@ -39,9 +39,64 @@ public class Main {
 
   private static void preInit(FluoConfiguration fluoConfig) {
     //this method does not need to be changed for earlier excercises in tour
+    //TODO try adjusting max tx mem commit mem
   }
 
   private static void excercise(MiniFluo mini, FluoClient client) {
-    //TODO Do all Fluo Tour excercises
+    
+    
+    Column cols[] = new Column[]{new Column("fam1","qual1"), new Column("fam1","qual2"), new Column("fam1","qual3")};
+
+    int count = 0;
+    long t1 = System.currentTimeMillis();
+    for (int k = 0; k < 1000; k++) {
+      
+      try (LoaderExecutor le = client.newLoaderExecutor()) {
+        for (int i = 0; i < 10; i++) {
+          final int start = count;
+          count+=1000;
+          le.execute((tx, ctx) -> {
+            for (int j = 0; j < 333; j++) {
+              String row = String.format("%06x", j + start);
+              tx.set(row, cols[0], Integer.toHexString(Math.abs(row.hashCode())));
+              tx.set(row, cols[1], Integer.toHexString(Math.abs(row.hashCode()*3)));
+              tx.set(row, cols[2], Integer.toHexString(Math.abs(row.hashCode()*5)));
+            }
+          });
+        }
+      }
+    }
+    long t2 = System.currentTimeMillis();
+    System.out.println("time1 " + (t2 - t1));
+    
+    HashSet<Column> colSet = new HashSet<>();
+    colSet.add(cols[0]);
+    colSet.add(cols[2]);
+    colSet.add(new Column("fam1","qual4"));
+    
+    for(int i = 0; i< 100; i++){
+      scan(client, cols, colSet);
+    }
+  }
+
+  private static void scan(FluoClient client, Column[] cols, HashSet<Column> colSet) {
+    long t1;
+    long t2;
+    try(Snapshot snap = client.newSnapshot()) {
+      t1 = System.currentTimeMillis();
+      CellScanner cellScanner = snap.scanner().fetch(cols[0],cols[2]).build();
+      int count = 0;
+      for (RowColumnValue rcv : cellScanner) {
+       if(colSet.contains(rcv.getColumn())){
+         count++;
+       }
+       
+       rcv.getRow().hashCode();
+      }
+      
+      System.out.println("count "+count);
+      t2 = System.currentTimeMillis();
+      System.out.println("time2 "+(t2 - t1));
+    }
   }
 }
